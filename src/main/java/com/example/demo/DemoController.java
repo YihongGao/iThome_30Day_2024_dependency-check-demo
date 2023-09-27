9f0c809b572ef2b3fbb5c9803cbb50f0869fa987
LeoKao <yihung.kao@advancegroup.com>
2023-09-28 00:45:13 +0800

    added print logs API

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     // 從 application.yml / app.welcome.message 取此變數值
     @Value("${app.welcome.message}")
@@ -32,5 +39,11 @@ public class DemoController {
     @RequestMapping("/token")
     public String token() {
         return token;
+    }
+
+    @PostMapping("/log-message")
+    public void logMessage(@RequestBody String message) {
+        // 將訊息記錄到日誌中
+        logger.info("接收到訊息：{}", message);
     }
 }
