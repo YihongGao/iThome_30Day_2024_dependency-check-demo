7f778d667b416b80f96dea7ea7dab2069b74b44c
LeoKao <leo.kao@pklotcorp.com>
2024-09-29 21:22:58 +0800

    add error endpoint

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -4,8 +4,8 @@ import com.example.demo.vo.Product;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +50,11 @@ public class DemoController {
     public Product product(@PathVariable("id") long id) {
         logger.info("productURL: " + productURL);
         return productClient.get().uri("/productInfo/{id}", id).retrieve().bodyToMono(Product.class).block();
+    }
+
+    @GetMapping("/error-api")
+    public ResponseEntity<String> error() {
+        return ResponseEntity.internalServerError().body("mock error");
     }
 
 
